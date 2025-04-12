3fb68b4142b628a5a3010564834c6c8dddbedb23
halley <halleychiky@gmail.com>
2025-04-12 23:46:34 +0200

    done for now

--- src/IOStream.java
+++ src/IOStream.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class IOStream {
     public static void main(String[] args){
         try (var br = new BufferedReader(new FileReader("file.txt"))) {
+            System.out.println(br.lines());
         }catch (FileNotFoundException e){
             System.out.println("file not found");
         }catch (IOException e){
