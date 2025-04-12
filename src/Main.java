1fc6dd34aad5e053584205fa8df7c2f5ae7a4d57
halley <halleychiky@gmail.com>
2025-04-12 23:46:43 +0200

    done for now

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Proxy;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
