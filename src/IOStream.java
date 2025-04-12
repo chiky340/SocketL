af41c1664abf4e679b3353d383e7ba31923e77a1
halley <halleychiky@gmail.com>
2025-04-12 22:32:31 +0200

    testing the filewriter

--- src/IOStream.java
+++ src/IOStream.java
@@ -1,5 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class IOStream {
     public static void main(String[] args){
-
+        try (FileWriter fw = new FileWriter("file.txt")) {
+            System.out.println("file has been made");
+        }catch (IOException e){
+            System.out.println("""
+                    couldnt write file
+                    """);
+        }
     }
 }
