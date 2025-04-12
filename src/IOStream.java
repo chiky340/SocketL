7acebc2d1c8c7ab156fad1659e8e40009cb936e6
halley <halleychiky@gmail.com>
2025-04-12 22:59:28 +0200

    now using the file reader

--- src/IOStream.java
+++ src/IOStream.java
@@ -2,16 +2,11 @@ import java.io.*;
 
 public class IOStream {
     public static void main(String[] args){
-        try (BufferedWriter fw = new BufferedWriter(new FileWriter("file.txt"))) {
-            for (int i=0;i<1000;i++) {
-                fw.write( i);
-                fw.newLine();
-            }
-            System.out.println("file has been made");
+        try (var br = new BufferedReader(new FileReader("file.txt"))) {
         }catch (FileNotFoundException e){
-            System.out.println("file was not found");
-        } catch (IOException e){
-            System.out.println("couldnt make file");
+            System.out.println("file not found");
+        }catch (IOException e){
+            System.out.println("file wasnt read");
         }
     }
 }
