9d09b477c7dccd0100765fc397aeb7db06f312d4
halley <halleychiky@gmail.com>
2025-04-12 22:34:49 +0200

    created a new file

--- src/IOStream.java
+++ src/IOStream.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -6,10 +7,10 @@ public class IOStream {
     public static void main(String[] args){
         try (FileWriter fw = new FileWriter("file.txt")) {
             System.out.println("file has been made");
-        }catch (IOException e){
-            System.out.println("""
-                    couldnt write file
-                    """);
+        }catch (FileNotFoundException e){
+            System.out.println("file was not found");
+        } catch (IOException e){
+            System.out.println("couldnt make file");
         }
     }
 }
