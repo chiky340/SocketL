08959bff3f66cd287a6b69f039e83ef8ed423414
halley <halleychiky@gmail.com>
2025-04-15 23:09:51 +0200

    made the socket that will make the communication possible

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,9 +1,14 @@
 import java.io.*;
+import java.net.ServerSocket;
 
 public class MyServer {
     public static void main(String[] args){
        public void start(final int portNumber){
-
+           try(var serverSocket = new ServerSocket(portNumber)){
+               var socket = serverSocket.accept();
+           }catch (IOException e){
+               throw new RuntimeException(e);
+           }
         }
 
     }
