a804dc98b21a53635cdc727a95a5b8da95578602
halley <halleychiky@gmail.com>
2025-04-15 23:12:15 +0200

    fixed the code

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,15 +1,14 @@
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 
 public class MyServer {
-    public static void main(String[] args){
-       public void start(final int portNumber){
-           try(var serverSocket = new ServerSocket(portNumber)){
-               var socket = serverSocket.accept();
-           }catch (IOException e){
-               throw new RuntimeException(e);
-           }
+    public void start(int portNumber) {
+        try (var serverSocket = new ServerSocket(portNumber)) {
+            var socket = serverSocket.accept();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
     }
+
 }
+
