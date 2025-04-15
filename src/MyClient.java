24927440462ff0050f321e658e7d8a055a7db34c
halley <halleychiky@gmail.com>
2025-04-15 23:35:10 +0200

    wrote catch for the Io Exception

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
 import java.net.Socket;
 
 public class MyClient {
     public void start(final int portNumber){
         try(var socket = new Socket("localHost",portNumber)){
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
