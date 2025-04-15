d581210c913d1be87e7507e0694edc59f6bc7fc6
halley <halleychiky@gmail.com>
2025-04-15 23:44:13 +0200

    fire code

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class MyClient {
     public void start(final int portNumber, final Scanner scanner){
-        try(var socket = new Socket("localHost",portNumber)){
+        try(var socket = new Socket("localHost",portNumber); var writer = new PrintWriter(socket.getOutputStream())){
             System.out.println("socket created");
-            for(String userInput; !(userInput = scanner.nextLine()).isEmpty())
+            for(String userInput; !(userInput = scanner.nextLine()).isEmpty();){
+
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
