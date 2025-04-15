2ad811efd32a7507d822dfef07c1ee9fd6c1580c
halley <halleychiky@gmail.com>
2025-04-15 23:03:25 +0200

    made a new start method in the server class

--- src/MyServer.java
+++ src/MyServer.java
@@ -2,12 +2,8 @@ import java.io.*;
 
 public class MyServer {
     public static void main(String[] args){
-        try (var br = new BufferedReader(new FileReader("file.txt"))) {
-            System.out.println(br.lines());
-        }catch (FileNotFoundException e){
-            System.out.println("file not found");
-        }catch (IOException e){
-            System.out.println("file wasnt read");
+       public void start(final int portNumber){
+
         }
 
     }
