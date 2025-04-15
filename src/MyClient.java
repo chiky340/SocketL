1572c51b0d6b20eac233ff4fbdc8e80aeac2ba41
halley <halleychiky@gmail.com>
2025-04-15 23:34:39 +0200

    writing the logic for the start in the myclient class

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,2 +1,9 @@
+import java.net.Socket;
+
 public class MyClient {
+    public void start(final int portNumber){
+        try(var socket = new Socket("localHost",portNumber)){
+
+        }
+    }
 }
