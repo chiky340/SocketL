f2c84f6af4804cc9c08ea1409c3026f7a077e589
halley <halleychiky@gmail.com>
2025-04-15 23:42:06 +0200

    dependency injection

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyClient {
-    public void start(final int portNumber){
+    public void start(final int portNumber, final Scanner scanner){
         try(var socket = new Socket("localHost",portNumber)){
             System.out.println("socket created");
+            for(String userInput; !(userInput = scanner.nextLine()).isEmpty())
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
