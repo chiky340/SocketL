aaff89d12e88a475e15b8b7ac95ed283212f454b
halley <halleychiky@gmail.com>
2025-05-07 21:10:06 +0200

    restarting to learn sockets

--- src/MyClient.java
+++ src/MyClient.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 
 public class MyClient {
     public void start(final int portNumber, final Scanner scanner){
-        try(var socket = new Socket("localHost",portNumber); var writer = new PrintWriter(socket.getOutputStream())){
+        try(var socket = new Socket("localHost",portNumber);
+            var writer = new PrintWriter(socket.getOutputStream(),true)){
             System.out.println("socket created");
             for(String userInput; !(userInput = scanner.nextLine()).isEmpty();){
-
+                writer.println(userInput);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
