f36cf2eeb6f7e0b2515d22e5755c3903460982e8
halley <halleychiky@gmail.com>
2025-05-07 21:58:03 +0200

    made a client class

--- src/client.java
+++ src/client.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class client {
@@ -7,6 +8,11 @@ public class client {
         var socket = new Socket("localhost",1234);
 
         var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        var out = new PrintWriter(socket.getOutputStream());
 
+        out.println("hello from server");
+
+        var response = in.readLine();
+        System.out.println("server says: "+ response);
     }
 }
