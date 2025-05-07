9e34ed44a8aeaf69644e90b647ffb1eb0ed20497
halley <halleychiky@gmail.com>
2025-05-07 21:10:01 +0200

    restarting to learn sockets

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,6 @@ public class Main {
     public static void main(String[] args) {
         final int portNumber = 12345;
 
-        try(var scanner = new Scanner(System.in)){
-            System.out.println("is this a sever yes or no");
-            if (scanner.nextLine().equalsIgnoreCase("y")){
-                new MyServer().start(portNumber);
-            }else{
-                new MyClient().start(portNumber,scanner);
-            }
-        }
+
     }
 }
