78faaf7711618fdd0caf42fcbc2083aa9e389944
halley <halleychiky@gmail.com>
2025-05-07 21:26:49 +0200

    writing some fire code

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Proxy;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        final int portNumber = 12345;
+    public static void main(String[] args) throws IOException {
+        var server = new ServerSocket(1234);
+        System.out.println("server is waiting");
 
+        Socket client = server.accept();
+        System.out.println("client connected");
+
+        var in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        var out = new PrintWriter(client.getOutputStream(),true);
+
+        var message = in.readLine();
 
     }
 }
