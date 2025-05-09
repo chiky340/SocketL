e9707ea937ba4d779672d4543976a8001f0e0962
halley <halleychiky@gmail.com>
2025-05-09 23:03:45 +0200

    making some big changes

--- src/client.java
+++ src/client.java
@@ -1,26 +1,16 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class client {
     public static void main(String[] args)throws Exception {
-        Socket socket = new Socket("localhost", 1234); // Connect to server
+        try(var socket = new Socket("localhost",1234)){
+            var outS = socket.getOutputStream();
+            var fileW = new PrintWriter(new OutputStreamWriter(outS , StandardCharsets.UTF_8));
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-        var scanner = new Scanner(System.in);
-        while(true){
-            var input = scanner.next();
-            if (input.equals("close"))
-                break;
-            else {
-                out.println(input);
-            }
+            fileW.println("hello world");
+            fileW.flush();
         }
-
-        socket.close();
     }
 }
