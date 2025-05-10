ce66bf2e3f6272f52c441e8644f012c0e8ae11fc
halley <halleychiky@gmail.com>
2025-05-10 22:40:39 +0200

    making some changes

--- src/client.java
+++ src/client.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 
 public class client {
     public static void main(String[] args)throws Exception {
-        try(var socket = new Socket("localhost",1234)){
+        try(var socket = new Socket("localhost",1235)){
             var outS = socket.getOutputStream();
             var fileW = new PrintWriter(new OutputStreamWriter(outS , StandardCharsets.UTF_8));
 
             var scan = new Scanner(System.in);
             while(true) {
                 var input = scan.nextLine();
-                if(!(input.equalsIgnoreCase("close"))) {
-                fileW.println(input);
-                fileW.flush();
-                }else {
-                    fileW.println("closing");
-                    fileW.flush();
+                if(input.equalsIgnoreCase("close")) {
+                    System.out.println("closing");
                     break;
+                }else {
+                    fileW.println(input);
+                    fileW.flush();
                 }
             }
         }
