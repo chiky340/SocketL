f6ff54a1ab6a959de074663131a5dfac29fc9672
halley <halleychiky@gmail.com>
2025-05-15 22:46:03 +0200

    using a thread to do all the work with the sockets

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -23,7 +24,11 @@ public class Main {
                     }
                 }).start();
                 new Thread(()->{
-
+                    try {
+                        var wr = new PrintWriter(clientSocket.getOutputStream());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }).start();
             }
         } catch (IOException e) {
