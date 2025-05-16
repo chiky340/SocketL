5883fd1643938a5106652a72976a1c9f272483a1
halley <halleychiky@gmail.com>
2025-05-16 21:36:33 +0200

    some of my code is finally working

--- src/client.java
+++ src/client.java
@@ -2,25 +2,30 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+
 public class client {
     public static void main(String[] args)throws Exception {
         try(var socket = new Socket("localhost",1235)){
             var outS = socket.getOutputStream();
             var fileW = new PrintWriter(new OutputStreamWriter(outS , StandardCharsets.UTF_8));
+            var fileIn = new BufferedReader(new InputStreamReader(socket.getInputStream(),StandardCharsets.UTF_8))
 
             var scan = new Scanner(System.in);
             while(true) {
-                new Thread(()->{
-                    BufferedWriter read = null;
-                    try {
-                        read = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                        read.write("hello");
-                        read.flush();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                var input = scan.nextLine();
+                new Thread(() -> {
+                    if (input.equalsIgnoreCase("close")) {
+                        System.out.println("closing");
+                    } else {
+                        fileW.println(input);
+                        fileW.flush();
                     }
                 }).start();
+                new Thread(()->{
+
+                }).start();
             }
+
         }
     }
 }
