93148571556356e5fa750dad5d5d9d2b1519582d
halley <halleychiky@gmail.com>
2025-05-16 22:35:45 +0200

    made a constructor

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,9 @@
+import java.net.ServerSocket;
+
 public class Server {
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
 }
