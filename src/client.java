1b7360c315d46d8a3529b8ab47d1717a988ccafa
halley <halleychiky@gmail.com>
2025-05-17 19:38:17 +0200

    cahnges

--- src/client.java
+++ src/client.java
@@ -8,7 +8,12 @@ public class client {
     private BufferedWriter bufferedWriter;
     private BufferedReader bufferedReader;
     private String username;
-    public client(Socket socket){
+    public client(Socket socket,String username){
+        try{
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }catch (IOException e)
 
     }
 }
