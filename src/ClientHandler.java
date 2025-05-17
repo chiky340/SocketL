75c7c35ddcf4b94a63b9ab8c43d116f6e8e3a191
halley <halleychiky@gmail.com>
2025-05-17 18:10:20 +0200

    added a constructor

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,4 +1,12 @@
+import java.net.Socket;
+
 public class ClientHandler implements Runnable{
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
     @Override
     public void run() {
 
