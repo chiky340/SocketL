49a922c0d02641bd7b79156f2c226809d0a8c350
halley <halleychiky@gmail.com>
2025-05-17 18:26:13 +0200

    made an aray list

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,7 +1,10 @@
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ClientHandler implements Runnable{
     private Socket socket;
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
