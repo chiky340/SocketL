3fc107516ec5ff38640c79b0211f0d18f13a703c
halley <halleychiky@gmail.com>
2025-05-17 18:25:00 +0200

    about to make some fire changes

--- src/client.java
+++ src/client.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class client {
     public static void main(String[] args)throws Exception {
-        try(var socket = new Socket("localhost",1235)){
+        try(var socket = new Socket("localhost",1234)){
             var outS = socket.getOutputStream();
             var fileW = new PrintWriter(new OutputStreamWriter(outS , StandardCharsets.UTF_8));
 
